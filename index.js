7e5d74551efe84a0b6a93e8e6696aa5b2edfe221
aalnoman <abdullahalnoman2042@gmail.com>
2023-06-07 18:07:41 +0600

    payment

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const jwt = require('jsonwebtoken');
 const cors = require('cors')
 require('dotenv').config()
+const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY)
 const port = process.env.PORT || 5000;
 
 // middelware
@@ -174,6 +175,19 @@ app.delete('/carts/:id',async(req, res)=>{
     res.send(result);
   })
 
+  // create payment intent
+app.post('/create-payment-intent',verifyJwt, async(req, res)=>{
+  const {price} = req.body;
+  const ammount = price*100 
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: ammount,
+    currency: "usd",
+    payment_method_types: ['card']
+  });
+  res.send({
+    clientSecret : paymentIntent.client_secret
+  })
+})
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
